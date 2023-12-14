66bfad0df5c97c55eab713e0d0f8dc77d167919b
bruno.morais <bruno.morais@auto1.com>
2023-12-14 15:06:57 +0100

    Add convertToJob

--- adapter/readjob.go
+++ adapter/readjob.go
@@ -13,19 +13,24 @@ func convertToJob(filePath string) []domain.Job {
 	var jobs []domain.Job
 	var job domain.Job
 	for _, line := range lines {
-		if strings.Contains("[", line) {
-			if &job == nil {
-				job = domain.Job{}
-				job.SetToken(strings.Trim(line, "[]"))
+		if strings.Contains(line, "[") {
+			if job.GetToken() == "" {
+				job.SetToken(getTokenFromLine(line))
 			} else {
 				jobs = append(jobs, job)
+				job = domain.Job{}
+				job.SetToken(getTokenFromLine(line))
 			}
 		} else {
 			job.AddExecutionCommand(line)
 		}
-
 	}
+	jobs = append(jobs, job)
 	return jobs
+}
+
+func getTokenFromLine(line string) string {
+	return strings.Trim(line, "[]")
 }
 
 func readFile(filePath string) []string {
---
+++ adapter/readjob_test.go
@@ -0,0 +1,18 @@
+package adapter
+
+import (
+	"testing"
+)
+
+func TestConvertToJob(t *testing.T) {
+	got := convertToJob("resources/testJobs.txt")
+	job := got[0]
+	jobToken := job.GetToken()
+	want := "myjob"
+	if jobToken != want {
+		t.Errorf("wanted [%s] but got [%s]", want, jobToken)
+	}
+	if len(got) != 2 {
+		t.Errorf("wanted 2 jobs but got %d", len(got))
+	}
+}
